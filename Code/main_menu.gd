380256e1c20aa64e1bfcebb3a9c8beea10006c3f
Marcel <mvelasquez2106@gmail.com>
2025-11-14 20:49:49 +0100

    Hovering

--- Code/main_menu.gd
+++ Code/main_menu.gd
@@ -6,6 +6,10 @@ var world = preload("res://scenes/World.tscn")
 @onready var menu = $UI/Menu
 @onready var level = $UI/LevelSelect
 
+@onready var left = $UI/LevelSelect/Left
+@onready var middle = $UI/LevelSelect/Middle
+@onready var right = $UI/LevelSelect/Right
+
 func _on_button_pressed() -> void:
 	menu.hide()
 	level.show()
@@ -19,15 +23,53 @@ func _on_left_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_released():
 		switch_scene(32)
 
+func _on_left_mouse_entered() -> void:
+	var t = get_tree().create_tween().set_parallel()
+	t.tween_property(left, "size", Vector2(382, 214), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.tween_property(left, "position", Vector2(150 - 44.5, 278 - 25.0), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+func _on_left_mouse_exited() -> void:
+	var t = get_tree().create_tween().set_parallel()
+	t.tween_property(left, "size", Vector2(293, 164), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.tween_property(left, "position", Vector2(150, 278), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+
 
 func _on_middle_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_released():
 		switch_scene(64)
 
+func _on_middle_mouse_entered() -> void:
+	var t = get_tree().create_tween().set_parallel()
+	t.tween_property(middle, "size", Vector2(382, 214), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.tween_property(middle, "position", Vector2(493 - 44.5, 278 - 25.0), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+func _on_middle_mouse_exited() -> void:
+	var t = get_tree().create_tween().set_parallel()
+	t.tween_property(middle, "size", Vector2(293, 164), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.tween_property(middle, "position", Vector2(493, 278), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+
 
 func _on_right_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_released():
 		switch_scene(128)
+
+func _on_right_mouse_entered() -> void:
+	var t = get_tree().create_tween().set_parallel()
+	t.tween_property(right, "size", Vector2(382, 214), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.tween_property(right, "position", Vector2(836 - 44.5, 278 - 25.0), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+func _on_right_mouse_exited() -> void:
+	var t = get_tree().create_tween().set_parallel()
+	t.tween_property(right, "size", Vector2(293, 164), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.tween_property(right, "position", Vector2(836, 278), 0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+
+func _on_back_pressed() -> void:
+	menu.show()
+	level.hide()
+
 
 func switch_scene(pixels: int):
 	var new_scene = world.instantiate()
