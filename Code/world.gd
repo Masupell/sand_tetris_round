093975680a4233b89478301173bff6f6461a4c7f
Marcel <mvelasquez2106@gmail.com>
2025-11-14 16:19:11 +0100

    UI in Game

--- Code/world.gd
+++ Code/world.gd
@@ -6,7 +6,13 @@ var pause: bool = false
 
 @onready var grid = $Grid
 @onready var blur = $Blur
-@onready var score_label = $Score
+@onready var score_label_one = $Score
+
+@onready var ui = $CanvasLayer/UI
+@onready var score_label_two = $CanvasLayer/UI/MarginContainer/VBoxContainer/Score
+@onready var high_score_label = $CanvasLayer/UI/MarginContainer/VBoxContainer/Highscore
+
+var high_score := 0
 
 func _ready() -> void:
 	randomize()
@@ -24,11 +30,28 @@ func _input(event: InputEvent) -> void:
 
 func toggle_pause(paused: bool):
 	if paused:
+		score_label_one.hide()
 		blur.show()
+		ui.show()
 	else:
+		score_label_one.show()
 		blur.hide()
+		ui.hide()
 
 func _physics_process(delta: float) -> void:
 	if !pause:
 		grid.update(delta)
-		score_label.text = str(grid.score/10) # Better with signal probably, but works for now
+		if grid.score > high_score:
+			high_score = grid.score
+		score_label_one.text = str(grid.score/10) # Better with signal probably, but works for now
+		score_label_two.text = "  Score: %d" % (grid.score / 10)
+		@warning_ignore("integer_division")
+		high_score_label.text = " HighScore: %d" % (high_score/10)
+
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
